64ecd56fff8b6bb74dc2b9ece56b3805f9b1347d
Frank Panetta <frank.panetta@entrendipity.com.au>
2013-02-17 01:55:19 +1100

    rewrite in TypeScript with Q

---
+++ tests/testacular.conf.js
@@ -0,0 +1,67 @@
+// Testacular configuration
+// Generated on Mon Feb 11 2013 21:00:20 GMT+1100 (EST)
+
+
+// base path, that will be used to resolve files and exclude
+basePath = '../';
+
+
+// list of files / patterns to load in the browser
+files = [
+  JASMINE,
+  JASMINE_ADAPTER,
+  '*.js',
+  'tests/*.spec.js'
+];
+
+
+// list of files to exclude
+exclude = [
+
+];
+
+
+// test results reporter to use
+// possible values: 'dots', 'progress', 'junit'
+reporters = ['progress'];
+
+
+// web server port
+port = 8080;
+
+
+// cli runner port
+runnerPort = 9100;
+
+
+// enable / disable colors in the output (reporters and logs)
+colors = true;
+
+
+// level of logging
+// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+logLevel = LOG_INFO;
+
+
+// enable / disable watching file and executing tests whenever any file changes
+autoWatch = true;
+
+
+// Start these browsers, currently available:
+// - Chrome
+// - ChromeCanary
+// - Firefox
+// - Opera
+// - Safari (only Mac)
+// - PhantomJS
+// - IE (only Windows)
+browsers = ['Chrome'/*, 'Firefox'*/];
+
+
+// If browser does not capture in given timeout [ms], kill it
+captureTimeout = 5000;
+
+
+// Continuous Integration mode
+// if true, it capture browsers, run tests and exit
+singleRun = false;
