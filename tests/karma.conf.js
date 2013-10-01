caffdb1eb9313add19b84d78dc29f60ecccb76ae
Frank Panetta <frank.panetta@entrendipity.com.au>
2013-10-01 14:35:16 +1000

    updating tests

--- tests/karma.conf.js
+++ tests/karma.conf.js
@@ -1,69 +1,72 @@
-// Karma configuration
-// Generated on Mon Apr 22 2013 16:54:40 GMT+1000 (EST)
+module.exports = function(config) {
+  config.set({
+	// Karma configuration
+	// Generated on Mon Apr 22 2013 16:54:40 GMT+1000 (EST)
 
 
-// base path, that will be used to resolve files and exclude
-basePath = '../../';
+	// base path, that will be used to resolve files and exclude
+	basePath: '../../',
+
+	frameworks: ["mocha"],
+	// list of files / patterns to load in the browser
+	files: [
+	  'helios/tests/chai.js',
+	  'helios/lib/sax.js',
+	  'helios/lib/heliosDB.js',
+	  'helios/tests/*.js'
+	],
 
 
-// list of files / patterns to load in the browser
-files = [
-  MOCHA,
-  MOCHA_ADAPTER,
-  'helios/tests/chai.js',
-  'helios/lib/sax.js',
-  'helios/lib/heliosDB.js',
-  'helios/tests/*.js'
-];
+	// list of files to exclude
+	exclude: [
+	  'helios/tests/karma.conf.js'
+	],
 
 
-// list of files to exclude
-exclude = [
-  'helios/tests/karma.conf.js'
-];
+	// test results reporter to use
+	// possible values: 'dots', 'progress', 'junit'
+	reporters: ['dots'],
 
 
-// test results reporter to use
-// possible values: 'dots', 'progress', 'junit'
-reporters = ['dots'];
+	// web server port
+	port: 9876,
 
 
-// web server port
-port = 9876;
+	// cli runner port
+	//runnerPort: 9100,
 
 
-// cli runner port
-runnerPort = 9100;
+	// enable / disable colors in the output (reporters and logs)
+	colors: true,
 
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
+	// level of logging
+	// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+	logLevel: config.LOG_INFO,
 
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+	// enable / disable watching file and executing tests whenever any file changes
+	autoWatch: true,
 
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = true;
+	// Start these browsers, currently available:
+	// - Chrome
+	// - ChromeCanary
+	// - Firefox
+	// - Opera
+	// - Safari (only Mac)
+	// - PhantomJS
+	// - IE (only Windows)
+	browsers: ['Chrome'],
 
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['Chrome'];
+	// If browser does not capture in given timeout [ms], kill it
+	captureTimeout: 60000,
 
 
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 60000;
+	// Continuous Integration mode
+	// if true, it capture browsers, run tests and exit
+	singleRun: false
 
-
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+  });
+};
